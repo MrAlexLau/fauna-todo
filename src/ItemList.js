7f1e53549581a94cc4afd623d37818da5666f015
MrAlexLau <laux0083@gmail.com>
2020-08-08 10:51:29 -0500

    Mark items as complete

--- src/ItemList.js
+++ src/ItemList.js
@@ -8,6 +8,7 @@ const ITEMS_QUERY = gql`
       data {
         _id
         name
+        isComplete
       }
     }
   }
@@ -21,12 +22,23 @@ const DELETE_ITEM = gql`
   }
 `;
 
+const UPDATE_ITEM = gql`
+  mutation UpdateItem($id: ID!, $isComplete: Boolean!) {
+    updateItem(id: $id, data: { isComplete: $isComplete }) {
+      _id
+      isComplete
+    }
+  }
+`;
+
 export function ItemList() {
   const { data, loading } = useQuery(ITEMS_QUERY);
 
   const [deleteItem, { loading: deleteLoading }] = useMutation(DELETE_ITEM, {
     refetchQueries: [{ query: ITEMS_QUERY }],
   });
+
+  const [updateItem, { loading: updateLoading }] = useMutation(UPDATE_ITEM);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -35,11 +47,27 @@ export function ItemList() {
   return (
     <ul>
       {data.allItems.data.map((item) => {
+        const labelStyles = item.isComplete
+          ? { style: { textDecoration: "line-through" } }
+          : {};
         return (
           <li key={item._id}>
-            {item.name}{" "}
+            <span {...labelStyles}>{item.name}</span>
             <button
-              disabled={deleteLoading}
+              disabled={deleteLoading || updateLoading}
+              style={{ marginLeft: "10px" }}
+              onClick={(e) => {
+                e.preventDefault();
+                updateItem({
+                  variables: { id: item._id, isComplete: !item.isComplete },
+                });
+              }}
+            >
+              {item.isComplete ? "Mark Incomplete" : "Mark Complete"}
+            </button>
+            <button
+              disabled={deleteLoading || updateLoading}
+              style={{ marginLeft: "10px" }}
               onClick={(e) => {
                 e.preventDefault();
                 deleteItem({ variables: { id: item._id } });
